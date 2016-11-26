95014c210b581daadbe858794c97acc27cbd7594
Mike Weaver <mweaver@invoca.com>
2016-11-25 21:47:33 -0800

    Remove unused SMTP settings

--- config/application.rb
+++ config/application.rb
@@ -23,17 +23,6 @@ module GitConflictDetector
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # SMTP settings
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address:              Rails.application.secrets.smtp_address,
-      port:                 587,
-      user_name:            Rails.application.secrets.smtp_user_name,
-      password:             Rails.application.secrets.smtp_password,
-      authentication:       'plain',
-      enable_starttls_auto: true
-    }
-
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     config.exceptions_app = routes
