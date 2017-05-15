04876d0fc9ff4854fa72aef784f23bb330072d47
Mike Weaver <mweaver@invoca.com>
2017-05-15 15:42:46 -0700

    Undo an overzelous Rubocop violation fix
    updated_record_count is a Fixnum, which does not respond to positive?

--- app/controllers/jira/status/push_controller.rb
+++ app/controllers/jira/status/push_controller.rb
@@ -49,7 +49,7 @@ module Jira
         updated_record_count = update_ignored_jira_issues(jira_issue_keys_to_ignore) + \
                                update_ignored_commits(commit_shas_to_ignore)
 
-        if updated_record_count.positive?
+        if updated_record_count > 0
           flash[:alert] = 'Push updated, refreshing JIRA and Git data'
           PushChangeHandler.new.process_push!(@push.id)
         elsif params['refresh']
