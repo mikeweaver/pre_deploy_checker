549dff925d52f127f5bd081ff24bbf9c784b53af
Christian Parkinson <cparkinson@invoca.com>
2020-09-15 12:58:43 -0400

    TECH-4799: Eager load lib/

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module GitConflictDetector
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
 
     config.exceptions_app = routes
 
