0e87e1a2b8fde3fdebac561148320223f8cc2605
Luan Vieira <lvieira@invoca.com>
2020-09-24 14:41:57 -0700

    TECH-4823: add Invoca Secrets key for service api

--- spec/controllers/api/callbacks/service_controller_spec.rb
+++ spec/controllers/api/callbacks/service_controller_spec.rb
@@ -10,22 +10,22 @@ module Api
       end
 
       let(:new_sha_value) { "bb8d05495e55a2f2311ccfe9521be955ca7d6395" }
+      let(:request_params) {{ ref: new_sha_value }}
+      let(:service_name) {{ service_name: "web" }}
       subject { Service.find_by(name: "web") }
 
       describe "POST #update" do
         it "returns success if service name is valid" do
-          params = { service_name: "web", ref: new_sha_value }
-          set_auth_header(params)
-          post :update, params: params
+          set_auth_header(request_params)
+          post :update, params: service_name.merge(request_params)
 
           expect(response).to have_http_status(200)
           expect(subject.ref).to eq(new_sha_value)
         end
 
         it "returns bad request if service name is not valid" do
-          params = { service_name: "coconuts", ref: new_sha_value }
-          set_auth_header(params)
-          post :update, params: params
+          set_auth_header(request_params)
+          post :update, params: { service_name: "coconuts" }.merge(request_params)
 
           expect(response).to have_http_status(404)
           expect(JSON.parse(response.body)).to eq({ "error" => "Not Found" })
@@ -33,33 +33,31 @@ module Api
       end
 
       describe "Authorization" do
-        let(:params) { { service_name: "we_be_authing", ref: "hey" } }
-
         it "allows requests that have the correct MAC in the Authorization header" do
-          set_auth_header(params)
-          post :update, params: params
+          set_auth_header(request_params)
+          post :update, params: service_name.merge(request_params)
 
           expect(response).to have_http_status(200)
         end
 
         it "rejects requests that have incorrect MAC in the Authorization header" do
-          digest = set_auth_header(params)
-          set_auth_header(params, mac: '1234')
-          post :update, params: params
+          digest = set_auth_header(request_params)
+          set_auth_header(request_params, mac: '1234')
+          post :update, params: service_name.merge(request_params)
 
           expect(response).to have_http_status(401)
           expect(JSON.parse(response.body)).to eq({ "error" => "Unauthorized Request: Authorization Header '1234' does not match Computed Digest '#{digest}'" })
         end
 
         it "rejects requests that do not have the Authorization header" do
-          post :update, params: params
+          post :update, params: service_name.merge(request_params)
 
           expect(response).to have_http_status(401)
           expect(JSON.parse(response.body)).to eq({ "error" => "Unauthorized Request: Missing Authorization header" })
         end
       end
 
-      def set_auth_header(body, mac: nil, secret_key: InvocaSecrets['pre_deploy_checker', 'secret_key'])
+      def set_auth_header(body, mac: nil, secret_key: InvocaSecrets['pre_deploy_checker', 'api', 'auth_key'])
         @request.env['HTTP_AUTHORIZATION'] = mac || OpenSSL::HMAC.hexdigest('SHA256', secret_key, body.deep_stringify_keys.sort.to_s)
       end
     end
